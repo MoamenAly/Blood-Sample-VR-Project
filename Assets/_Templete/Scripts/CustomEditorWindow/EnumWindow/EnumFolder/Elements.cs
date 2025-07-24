9d61065291eba029ec54170b4959f59bb5d69054
MoamenAly <moamen.aly@mindsxi.com>
2025-07-25 02:13:23 +0300

    Add Some Interactions

--- Assets/_Templete/Scripts/CustomEditorWindow/EnumWindow/EnumFolder/Elements.cs
+++ Assets/_Templete/Scripts/CustomEditorWindow/EnumWindow/EnumFolder/Elements.cs
@@ -4,14 +4,9 @@ public enum Elements
 {
 	Player_body = 0,
 	Player_head = 1,
-	Hand = 10,
-	Drops = 11,
-	NonSterlieGoves = 12,
-	SterlieGoves = 13,
-	TpnPort = 14,
-	GlucozHole = 15,
-	FlushGlocozJac = 16,
 	mask,
 	Gloves,
 	Coat,
+	CoholCOllider,
+	WrongCollisionNeedle
 }
